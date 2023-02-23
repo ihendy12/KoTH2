e2e5e108e576a7a08a4a66e2e8b30f1002fb39d0
Ian Henderson <ian.henderson1215@gmail.com>
2023-02-23 14:00:15 -0800

    Completed logic for Dragon and Werewolf classes to be able to fight back against. Game technically is completed now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -55,6 +55,7 @@ public class Main {
                     System.out.println("\n" + zombie.getName() + " hit for " + zBaseAttack + " damage");
                     System.out.println(hero.getName() + " has " + heroHealthBar + " health left!");
 
+                    // logic to check hero and zombie have health left
                     if (zHealth > 0){
                         zHealth -= base;
                     }
@@ -63,6 +64,8 @@ public class Main {
                     }
 
             } while (zHealth >= 0 && hHealth >= 0);
+            //Display winners
+
             if (hHealth <= 0){
                 System.out.println("\nThe mighty hero " + hero.getName() + " was defeated by " + zombie.getName());
             }
@@ -74,36 +77,78 @@ public class Main {
             System.out.println("You will be fighting: The Dragon");
             TimeUnit.SECONDS.sleep(2);
             do {
+                //Hero Attacks
                 int base = hero.getBaseAttack();
                 int dragonHealthBar = dHealth - base;
                 System.out.println(hero.getName() + " hit for " + base + " damage");
                 System.out.println(dragon.getName() + " has " + dragonHealthBar + " health left!");
+
+                //Dragon attack
+                int dBaseAttack = dragon.getBaseAttack();
+                int heroHealthBar = hHealth - dBaseAttack;
+                System.out.println("\n" + dragon.getName() + " hit for " + dBaseAttack + " damage");
+                System.out.println(hero.getName() + " has " + heroHealthBar + " health left!");
+
+                // logic to check hero and dragon have health left
                 if (dHealth > 0){
                     dHealth -= base;
                 }
-            } while (dHealth >= 0);
-            System.out.println("you win!");
+                if (hHealth > 0){
+                    hHealth -= dBaseAttack;
+                }
+
+            } while (dHealth >= 0 && hHealth >= 0);
+            //Display winner between dragon and hero
+            if (hHealth <= 0){
+                System.out.println("\nThe mighty hero " + hero.getName() + " was defeated by " + dragon.getName());
+            }
+
+            if (dHealth <= 0){
+                System.out.println("\n" + dragon.getName() + " was defeated by the mighty hero " + hero.getName());
+            }
 
 
         } else if (randomOpponent == 3) {
             System.out.println("You will be fighting: The Werewolf");
             TimeUnit.SECONDS.sleep(2);
             do {
+
+                //Hero Attack
                 int base = hero.getBaseAttack();
                 int werewolfHealthBar = wHealth - base;
                 System.out.println(hero.getName() + " hit for " + base + " damage");
                 System.out.println(werewolf.getName() + " has " + werewolfHealthBar + " health left!");
+
+                //Werewolf attack
+
+                int wBaseAttack = werewolf.getBaseAttack();
+                int heroHealthBar = hHealth - wBaseAttack;
+                System.out.println("\n" + werewolf.getName() + " hit for " + wBaseAttack + " damage");
+                System.out.println(hero.getName() + " has " + heroHealthBar + " health left!");
+
                 if (wHealth > 0) {
                     wHealth -= base;
                 }
-            } while (wHealth >= 0);
-            System.out.println("you win!");
+                if (hHealth > 0) {
+                    hHealth -= wBaseAttack;
+                }
+            } while (wHealth >= 0 && hHealth >= 0);
+            //Display winner
+
+            if (hHealth <= 0){
+                System.out.println("\nThe mighty hero " + hero.getName() + " was defeated by " + werewolf.getName());
+            }
+
+            if (wHealth <= 0){
+                System.out.println("\n" + werewolf.getName() + " was defeated by the mighty hero " + hero.getName());
+            }
+
+
                 }
 
 
 
 
-            // Display winners
 
 
             }
